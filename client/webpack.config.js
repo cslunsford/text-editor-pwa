923de857acd8ac0a8be6913bc994cf0e626b6291
cslunsford <christopher.lunsford@uconn.edu>
2023-10-19 17:00:07 -0400

    fixing path to manifest.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'auto'),
+      path: path.resolve(__dirname, '/auto'),
     },
     plugins: [
       new HtmlWebpackPlugin({
